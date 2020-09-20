f6d5de9c8f83a8db14e5e9574081efe23977fdf4
Ekaterina <rina_ushakova@bk.ru>
2020-09-20 12:11:47 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+// const answers = [];
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = prompt("Как ваше отчество?", "");
+
+// document.write(answers);
+
+let incr = 10,
+    decr = 10;
+// incr++;
+// decr --;
+
+++incr;
+--decr;
+console.log(incr);
+
+
+
+
