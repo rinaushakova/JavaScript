93ebb8a5413d367306fe09c9f1806e796836f602
Ekaterina <rina_ushakova@bk.ru>
2020-09-22 10:25:40 +0200

    Added two more ways for conditions

--- js/script.js
+++ js/script.js
@@ -35,7 +35,34 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
+// let i = 0;
+// do {
+//    const  a = prompt("Один из последних просмотренных фильмов?", ""),
+//           b = prompt("На сколько оцените его?", "");
+//           i++;
+//     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log ('done');
+//     } else {
+//         console.log ('error');
+//         i--;
+//     }
+// } while (i < 2);
 
+
+// let i = 0;
+// while (i < 2) {
+//     const  a = prompt("Один из последних просмотренных фильмов?", ""),
+//            b = prompt("На сколько оцените его?", "");
+//            i++;
+//      if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//      personalMovieDB.movies[a] = b;
+//      console.log ('done');
+//      } else {
+//          console.log ('error');
+//          i--;
+//      }
+// }
 
 console.log(personalMovieDB);
 
